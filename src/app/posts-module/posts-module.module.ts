260be706d9510f110347a3097412cdfae3216cdd
timoshevichaleks <alti2692@gmail.com>
2021-09-14 22:04:42 +0300

    Fixed bugs

--- src/app/posts-module/posts-module.module.ts
+++ src/app/posts-module/posts-module.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostsComponent } from "./posts/posts.component";
-import { AppRoutingModule } from "../app-routing.module";
 import { PostComponent } from "./post/post.component";
+import { AppRoutingModule } from "../app-routing.module";
 
 
 @NgModule({
