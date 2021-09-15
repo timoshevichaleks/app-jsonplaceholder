bd1b161d1b973f4bb8a6da80efb74101947bad06
timoshevichaleks <alti2692@gmail.com>
2021-09-15 11:13:34 +0300

    Replacing imports

--- src/app/posts-module/posts-module.module.ts
+++ src/app/posts-module/posts-module.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostsComponent } from "./posts/posts.component";
 import { PostComponent } from "./post/post.component";
-import { AppRoutingModule } from "../app-routing.module";
+import { RouterModule } from "@angular/router";
 
 
 @NgModule({
@@ -15,7 +15,7 @@ import { AppRoutingModule } from "../app-routing.module";
   ],
   imports: [
     CommonModule,
-    AppRoutingModule
+    RouterModule
   ]
 })
 export class PostsModuleModule {
